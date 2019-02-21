fa1a073aae981e29fb968f0410ba973417af1e64
Tim Albert <talbert1209@gmail.com>
2019-02-21 13:34:27 -0600

    Add project files.

---
+++ Inheritance06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inheritance06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var player = new Player() { Name = "Bob", Strength = 20 };
+            var warrior = new Warrior() { Name = "Baltek", Strength = 100, Bonus = 10 };
+            var wizard = new Wizard() { Name = "Pentagorn", Strength = 50, Energy = 50 };
+
+            var players = new List<Player>();
+            players.Add(player);
+            players.Add(warrior);
+            players.Add(wizard);
+
+            DoBattle(players);
+
+            Console.ReadLine();
+        }
+
+        static void DoBattle(List<Player> players)
+        {
+            foreach (var player in players)
+            {
+                player.Attack();
+                Console.WriteLine("");
+            }
+        }
+    }
+}
+}
